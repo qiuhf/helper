0571a88f5d8375470d387a6fcce838fdb827dfbd
haifengQiu <sz_qiuhf@163.com>
2021-03-18 01:21:19 +0800

    1.新增JUC工程 2.线程启动的三种方式 3.线程状态间的切换

---
+++ helper-juc/src/main/java/com/hleper/juc/thread000/HowToCreateThread.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020-2021. the original qiuhaifeng .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hleper.juc.thread000;
+
+import java.util.concurrent.Executors;
+
+/**
+ * 启动线程的三种方式
+ * 1. 继承Thread类
+ * 2. 实现Runnable接口
+ * 3. 线程池方式
+ *
+ * @author sz_qiuhf@163.com
+ * @since 2021-03-17
+ **/
+public class HowToCreateThread {
+    public static void main(String[] args) {
+        System.out.println("Hello " + Thread.currentThread().getName());
+
+        MyThread thread = new MyThread();
+        // 设置线程名称
+        thread.setName("MyThread");
+        // run(), 可以理解: 主线程调用MyThread线程中run方法，没有真正启动线程，线程的状态：NEW
+        thread.run();
+        System.out.println("当前线程状态 = " + thread.getState().name());
+        // 启动线程
+        thread.start();
+        System.out.println("当前线程状态 = " + thread.getState().name());
+
+
+        Thread runnable = new Thread(new MyRunnable());
+        runnable.setName("MyRunnable");
+        runnable.start();
+
+        Executors.newCachedThreadPool().submit(() ->
+                System.out.println("Hello ThreadPool.")
+        );
+    }
+
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello " + Thread.currentThread().getName());
+        }
+    }
+
+    private static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Hello " + Thread.currentThread().getName());
+        }
+    }
+}
---
+++ helper-juc/src/main/java/com/hleper/juc/thread000/WhatIsThreadState.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2020-2021. the original qiuhaifeng .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hleper.juc.thread000;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程状态
+ *
+ * @author sz_qiuhf@163.com
+ * @since 2021-03-17
+ **/
+public class WhatIsThreadState {
+    /**
+     * 模拟线程被锁定场景
+     */
+    private static final Object LOCK = new Object();
+    /**
+     * 必须要加volatile，保证线程之间可见
+     */
+    private static volatile boolean stop = false;
+
+    public static void main(String[] args) throws Exception {
+        MyThread thread = new MyThread();
+        // MyThread线程初始状态：NEW
+        printCurrentThreadState(thread);
+        // 启动MyThread线程
+        thread.start();
+        // MyThread线程的状态：NEW -> RUNNABLE
+        printCurrentThreadState(thread);
+
+        // 休眠1s, 让MyThread先获取到锁, 模拟获取锁场景
+        TimeUnit.SECONDS.sleep(1);
+        // 没拿到LOCK锁，阻塞
+        synchronized (LOCK) {
+            // MyThread.run()调用了wait()，MyThread线程状态：RUNNABLE -> WAITING
+            printCurrentThreadState(thread);
+            // 获取锁
+            LOCK.notify();
+            // 主线程获取到锁，MyThread线程状态：WAITING -> BLOCKED
+            printCurrentThreadState(thread);
+
+            stop = true;
+        }
+
+        // MyThread.run()调用了Thread.sleep(), MyThread线程状态：BLOCKED -> TIMED_WAITING
+        printCurrentThreadState(thread);
+
+        // 等待MyThread线程执行完
+        thread.join();
+        // MyThread线程状态：TIMED_WAITING -> TERMINATED
+        printCurrentThreadState(thread);
+    }
+
+    private static void printCurrentThreadState(MyThread thread) {
+        System.out.println("My thread state: " + thread.getState().name());
+    }
+
+    private static class MyThread extends Thread {
+        @SneakyThrows
+        @Override
+        public void run() {
+            synchronized (LOCK) {
+                if (!stop) {
+                    LOCK.wait();
+                }
+            }
+            Thread.sleep(2000);
+        }
+    }
+}
